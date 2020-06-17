cde7a98e8fdd76106fdcb5ab36ac2e1b7a78ca5e
Matthijs van den Bos <matthijs@vandenbos.org>
2020-06-17 12:21:41 +0200

    Remove obsolete scripts

--- bin/run_c_unit_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd $(git rev-parse --show-toplevel)/pepper/apps
-
-if [ ! -f Makefile ]; then 
-    cmake . 
-fi
-make test
-#clang simple_contract_test.c -ldl -rdynamic -lcmocka -Ied25519 -std=c89 -o /tmp/simple_contract_test && /tmp/simple_contract_test && rm /tmp/simple_contract_test
