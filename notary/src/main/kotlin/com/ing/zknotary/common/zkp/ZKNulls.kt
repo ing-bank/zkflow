ecdcb6ca9a9e7c343102102d7de7da7303716532
Victor Ermolaev <victorermolaev@gmail.com>
2020-06-22 10:43:08 +0200

    Add null values to construct empty states

---
+++ notary/src/main/kotlin/com/ing/zknotary/common/zkp/ZKNulls.kt
@@ -0,0 +1,22 @@
+package com.ing.zknotary.common.zkp
+
+import net.corda.core.identity.AnonymousParty
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.identity.Party
+import java.security.PublicKey
+
+object ZKNulls {
+    object NullPublicKey : PublicKey, Comparable<PublicKey> {
+        override fun getAlgorithm() = "NULL"
+        override fun getEncoded() = ByteArray(44){0}
+        override fun getFormat() = "NULL"
+        override fun compareTo(other: PublicKey): Int = if (other == NullPublicKey) 0 else -1
+        override fun toString() = "NULL_KEY"
+    }
+
+    val NULL_ANONYNOUS_PARTY = AnonymousParty(NullPublicKey)
+    val NULL_PARTY = Party(CordaX500Name.parse("O=BOGUS,L=New York,C=US"), NullPublicKey)
+
+    /** A signature with a key and value of zero. Useful when you want a signature object that you know won't ever be used. */
+//    val NULL_SIGNATURE = TransactionSignature(ByteArray(32), NullPublicKey, SignatureMetadata(1, -1))
+}
