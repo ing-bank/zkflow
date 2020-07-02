817ee71c312096f0bc4b12439502583a1f5e3114
Victor Ermolaev <victorermolaev@gmail.com>
2020-07-02 09:38:48 +0200

    Update NULL_PUBLIC_KEY to be deserializable by AMQP.

--- notary/src/main/kotlin/com/ing/zknotary/common/zkp/ZKNulls.kt
+++ notary/src/main/kotlin/com/ing/zknotary/common/zkp/ZKNulls.kt
@@ -1,19 +1,14 @@
 package com.ing.zknotary.common.zkp
 
+import net.corda.core.crypto.Crypto
 import net.corda.core.identity.AnonymousParty
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.identity.Party
 import java.security.PublicKey
 
 object ZKNulls {
-    object NULL_PUBLIC_KEY : PublicKey, Comparable<PublicKey> {
-        override fun getAlgorithm() = "NULL"
-        override fun getEncoded() = ByteArray(44) { 0 }
-        override fun getFormat() = "NULL"
-        override fun compareTo(other: PublicKey): Int = if (other == NULL_PUBLIC_KEY) 0 else -1
-        override fun toString() = "NULL_KEY"
-    }
-
+    // TODO: Make a function accepting a signature scheme
+    val NULL_PUBLIC_KEY: PublicKey = Crypto.generateKeyPair().public
     val NULL_ANONYMOUS_PARTY = AnonymousParty(NULL_PUBLIC_KEY)
     val NULL_PARTY = Party(CordaX500Name.parse("O=BOGUS,L=New York,C=US"), NULL_PUBLIC_KEY)
 
