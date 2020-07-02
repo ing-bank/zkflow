e0923dd2d1e562d8de76af0004827eb403250007
Matthijs van den Bos <matthijs@vandenbos.org>
2020-07-02 18:25:12 +0200

    Add more shared run configs

---
+++ config.gradle.kts
@@ -0,0 +1,16 @@
+mapOf(
+    "cordaReleaseGroup" to "net.corda",
+    "cordaCoreReleaseGroup" to "net.corda",
+    "cordaVersion" to "4.5.6-ING",
+    "gradlePluginsVersion" to "5.0.4",
+    "kotlinVersion" to "1.3.72",
+    "junitVersion" to "4.12",
+    "quasarVersion" to "0.7.10",
+    "log4jVersion" to "2.11.2",
+    "platformVersion" to "5",
+    "slf4jVersion" to "1.7.25",
+    "nettyVersion" to "4.1.22.Final"
+).entries.forEach {
+    project.extra.set(it.key, it.value)
+}
+
