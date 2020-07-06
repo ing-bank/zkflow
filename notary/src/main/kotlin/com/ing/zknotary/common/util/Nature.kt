501838ef50cd5f490d72c81a7de268394fe50e44
Victor Ermolaev <victorermolaev@gmail.com>
2020-07-06 12:31:42 +0200

    Make Nature class more comprehensible

--- notary/src/main/kotlin/com/ing/zknotary/common/util/Nature.kt
+++ notary/src/main/kotlin/com/ing/zknotary/common/util/Nature.kt
@@ -1,16 +1,25 @@
 package com.ing.zknotary.common.util
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
+/**
+ * This class wraps a value of any type to mark contained data
+ * as either Authentic or Bogus.
+ */
 sealed class Nature<T> {
-    abstract val quality: Int
+    // see https://github.com/FasterXML/jackson-module-kotlin/issues/80
+    @get:JsonProperty("isBogus")
+    abstract val isBogus: Int
+
     abstract val content: T
 
     data class Authentic<T>(override val content: T) : Nature<T>() {
-        override val quality: Int
+        override val isBogus: Int
             get() = 1
     }
 
     data class Bogus<T>(override val content: T) : Nature<T>() {
-        override val quality: Int
+        override val isBogus: Int
             get() = 0
     }
 }
