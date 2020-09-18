bf01f2a732920ca7d787a86e5814598278675537
Victor Ermolaev <victorermolaev@gmail.com>
2020-09-18 22:16:52 +0200

    Clarify FingerprintableTypes

--- notary/src/main/kotlin/com/ing/zknotary/common/dactyloscopy/FingerprintableTypes.kt
+++ notary/src/main/kotlin/com/ing/zknotary/common/dactyloscopy/FingerprintableTypes.kt
@@ -10,16 +10,55 @@ import java.nio.ByteBuffer
 import java.security.PublicKey
 import java.time.Instant
 
-/*
- * Corda classes
+/**
+ * PLEASE READ before extending new types with the fingerprinting functionality.
+ *
+ * Extending types with the fingerprinting functionality is easy, but not always risk-free.
+ * Consider implication before amending this file.
+ *
+ * Java/Kotlin allow for complex class hierarchies, thus the type implementing or extending
+ * from any of the types extended here will inherit the fingerprinting functionality from the parent.
+ * Such behavior is not guaranteed to produce correct fingerprint for the children classes.
+ * It may happen that an implementing/extending class contains extra fields which must be accounted in
+ * its fingerprint, but these fields will be skipped and the parent fingerprinting functionality
+ * will be used. This argument demands higher attention to what types are extended with
+ * the fingerprinting functionality.
+ *
+ * Types that are neither implementable nor extendable are SAFE:
+ * 1. Types marked with the keyword `final` in Java,
+ * 2. *not* marked with the keyword `open` in Kotlin,
+ * 3. *not* marked with the keyword `abstract` in either language
+ *
+ * Interfaces and non final classes (`open` in Kotlin) are UNSAFE and must be extended with great care.
  */
+
+/*
+ * Safe classes.
+ */
+fun Int.fingerprint(): ByteArray =
+    ByteBuffer.allocate(4).putInt(this).array()
+
+fun ByteArray.fingerprint(): ByteArray =
+    this
+
+fun Instant.fingerprint(): ByteArray =
+    ByteBuffer.allocate(8).putLong(epochSecond).array() +
+        ByteBuffer.allocate(4).putInt(nano).array()
+
 fun StateRef.fingerprint(): ByteArray =
-    txhash.bytes + index.fingerprint()
+    txhash.fingerprint() + index.fingerprint()
 
 // FIXME: This is now ignoring some of the important fields of a TransactionState.
 fun <T> TransactionState<T>.fingerprint(): ByteArray
     where T : ContractState, T : Fingerprintable =
     data.fingerprint() + notary.owningKey.fingerprint()
+
+/*
+ * Unsafe types: interfaces and extendable classes
+ */
+
+fun PublicKey.fingerprint(): ByteArray =
+    this.encoded
 
 fun AbstractParty.fingerprint(): ByteArray =
     this.owningKey.fingerprint()
@@ -30,19 +69,3 @@ fun SecureHash.fingerprint(): ByteArray =
 fun TimeWindow.fingerprint(): ByteArray =
     (fromTime?.fingerprint() ?: ByteArray(12) { 0 }) +
         (untilTime?.fingerprint() ?: ByteArray(12) { 0 })
-
-/*
- * Core Java/Kotlin classes
- */
-fun Int.fingerprint(): ByteArray =
-    ByteBuffer.allocate(4).putInt(this).array()
-
-fun ByteArray.fingerprint(): ByteArray =
-    this
-
-fun PublicKey.fingerprint(): ByteArray =
-    this.encoded
-
-fun Instant.fingerprint(): ByteArray =
-    ByteBuffer.allocate(8).putLong(epochSecond).array() +
-        ByteBuffer.allocate(4).putInt(nano).array()
