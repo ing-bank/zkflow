b0b19edaf1d42e2954b53a53a83e2cbaafa07e8e
Victor Ermolaev <victorermolaev@gmail.com>
2020-10-05 16:35:13 +0200

    Move component padding configuration to commands definition

--- notary/src/main/kotlin/com/ing/zknotary/common/contracts/ZKContractState.kt
+++ notary/src/main/kotlin/com/ing/zknotary/common/contracts/ZKContractState.kt
@@ -1,6 +1,7 @@
 package com.ing.zknotary.common.contracts
 
 import com.ing.zknotary.common.dactyloscopy.Fingerprintable
+import com.ing.zknotary.common.util.ComponentPaddingConfiguration
 import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.ContractState
 import java.nio.ByteBuffer
@@ -9,6 +10,8 @@ interface ZKContractState : Fingerprintable, ContractState
 
 interface ZKCommandData : Fingerprintable, CommandData {
     val id: Int
+
+    val paddingConfiguration: ComponentPaddingConfiguration
 
     override fun fingerprint(): ByteArray =
         ByteBuffer.allocate(4).putInt(id).array()
--- notary/src/main/kotlin/com/ing/zknotary/common/transactions/ZKProverTransactionFactory.kt
+++ notary/src/main/kotlin/com/ing/zknotary/common/transactions/ZKProverTransactionFactory.kt
@@ -1,18 +1,13 @@
 package com.ing.zknotary.common.transactions
 
-import com.ing.zknotary.common.contracts.TestContract
 import com.ing.zknotary.common.contracts.ZKContractState
 import com.ing.zknotary.common.states.toZKCommand
-import com.ing.zknotary.common.util.ComponentPaddingConfiguration
-import com.ing.zknotary.common.zkp.ZKNulls
 import com.ing.zknotary.node.services.ZKTransactionStorage
 import net.corda.core.contracts.StateAndRef
 import net.corda.core.contracts.StateRef
 import net.corda.core.contracts.TransactionState
 import net.corda.core.contracts.requireThat
-import net.corda.core.crypto.BLAKE2s256DigestService
 import net.corda.core.crypto.DigestService
-import net.corda.core.crypto.SecureHash
 import net.corda.core.node.ServiceHub
 import net.corda.core.serialization.serialize
 import net.corda.core.transactions.LedgerTransaction
@@ -22,8 +17,7 @@ fun WireTransaction.toZKProverTransaction(
     services: ServiceHub,
     zkTransactionStorage: ZKTransactionStorage,
     componentGroupLeafDigestService: DigestService,
-    nodeDigestService: DigestService = componentGroupLeafDigestService,
-    componentPaddingConfiguration: ComponentPaddingConfiguration = ZKProverTransactionFactory.DEFAULT_PADDING_CONFIGURATION
+    nodeDigestService: DigestService = componentGroupLeafDigestService
 ): ZKProverTransaction {
     requireThat {
         // "A notary must always be set on a ZKProverTransaction" using (notary != null)
@@ -45,15 +39,14 @@ fun WireTransaction.toZKProverTransaction(
         inputs = ltx.inputs.mapToZkid(),
         outputs = ltx.outputs.map { TransactionState(data = it.data as ZKContractState, notary = it.notary) },
         references = ltx.references.mapToZkid(),
-        command = ltx.commands.map { it.toZKCommand() }.single(),
+        command = ltx.commands.single().toZKCommand(),
         notary = ltx.notary!!,
         timeWindow = ltx.timeWindow,
         privacySalt = ltx.privacySalt,
         networkParametersHash = ltx.networkParameters?.serialize()?.hash,
         attachments = ltx.attachments.map { it.id },
         componentGroupLeafDigestService = componentGroupLeafDigestService,
-        nodeDigestService = nodeDigestService,
-        componentPaddingConfiguration = componentPaddingConfiguration
+        nodeDigestService = nodeDigestService
     )
 }
 
@@ -63,8 +56,7 @@ class ZKProverTransactionFactory {
         fun create(
             ltx: LedgerTransaction,
             componentGroupLeafDigestService: DigestService,
-            nodeDigestService: DigestService,
-            componentPaddingConfiguration: ComponentPaddingConfiguration = DEFAULT_PADDING_CONFIGURATION
+            nodeDigestService: DigestService
         ): ZKProverTransaction {
             requireThat {
                 // "A notary must always be set on a ZKProverTransaction" using (ltx.notary != null)
@@ -82,31 +74,8 @@ class ZKProverTransactionFactory {
                 networkParametersHash = ltx.networkParameters?.serialize()?.hash,
                 attachments = ltx.attachments.map { it.id },
                 componentGroupLeafDigestService = componentGroupLeafDigestService,
-                nodeDigestService = nodeDigestService,
-                componentPaddingConfiguration = componentPaddingConfiguration
+                nodeDigestService = nodeDigestService
             )
-        }
-
-        val DEFAULT_PADDING_CONFIGURATION by lazy {
-            // TODO: Make this more generic, based on the contract and asking it to provide the padding state
-            val emptyState = TestContract.TestState(ZKNulls.NULL_PARTY, 0)
-            val transactionStateFiller = ComponentPaddingConfiguration.Filler.TransactionState(
-                TransactionState(emptyState, notary = ZKNulls.NULL_PARTY)
-            )
-            val stateAndRefFiller = ComponentPaddingConfiguration.Filler.StateAndRef(
-                StateAndRef(
-                    TransactionState(emptyState, notary = ZKNulls.NULL_PARTY),
-                    StateRef(BLAKE2s256DigestService.zeroHash, 0)
-                )
-            )
-
-            ComponentPaddingConfiguration.Builder()
-                .inputs(2, stateAndRefFiller)
-                .outputs(2, transactionStateFiller)
-                .references(2, stateAndRefFiller)
-                .attachments(2, ComponentPaddingConfiguration.Filler.SecureHash(SecureHash.zeroHash))
-                .signers(2)
-                .build()
         }
     }
 }
