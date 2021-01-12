257c86e6f7910ab78f65493ab59fc24236b782fa
Viktor Sergeev <viktor.sergeev@ing.com>
2021-01-12 14:10:49 +0100

    Add unit tests for comparison of Double and BigDecimal

--- notary/src/test/kotlin/com/ing/zknotary/common/input/DoubleComparisonTest.kt
+++ notary/src/test/kotlin/com/ing/zknotary/common/input/DoubleComparisonTest.kt
@@ -1,39 +1,274 @@
-package com.ing.zknotary.common.hashes
+package com.ing.zknotary.common.input
 
 import com.ing.zknotary.common.zkp.ZincZKService
 import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import java.time.Duration
 
 class DoubleComparisonTest {
-    private val circuitFolder: String = javaClass.getResource("/DoubleComparisonTest").path
-    private val zincZKService = ZincZKService(
-        circuitFolder,
-        artifactFolder = circuitFolder,
-        buildTimeout = Duration.ofSeconds(5),
-        setupTimeout = Duration.ofSeconds(300),
-        provingTimeout = Duration.ofSeconds(300),
-        verificationTimeout = Duration.ofSeconds(1)
-    )
 
-    init {
-        zincZKService.setup()
-    }
+    companion object {
+        private val circuitFolder: String = DoubleComparisonTest::class.java.getResource("/DoubleTest").path
+        private val zincZKService = ZincZKService(
+            circuitFolder,
+            artifactFolder = circuitFolder,
+            buildTimeout = Duration.ofSeconds(5),
+            setupTimeout = Duration.ofSeconds(300),
+            provingTimeout = Duration.ofSeconds(300),
+            verificationTimeout = Duration.ofSeconds(1)
+        )
 
-    @AfterAll
-    fun `remove zinc files`() {
-        zincZKService.cleanup()
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            zincZKService.setup()
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun `remove zinc files`() {
+            zincZKService.cleanup()
+        }
     }
 
     @Test
-    fun `zinc verifies double comparison`() {
+    fun `zinc compares two zeros`() {
+        // 0
+        val leftSign: Byte = 0
+        val leftExponent: Short = 0
+        val leftMagnitude: Long = 0
+
+        // 0
+        val rightSign: Byte = 0
+        val rightExponent: Short = 0
+        val rightMagnitude: Long = 0
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"0\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares positive without exponent with zero`() {
+        // 10
+        val leftSign: Byte = 1
+        val leftExponent: Short = 0
+        val leftMagnitude: Long = 10
+
+        // 0
+        val rightSign: Byte = 0
+        val rightExponent: Short = 0
+        val rightMagnitude: Long = 0
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"1\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares negative without exponent with zero`() {
         // -10
         val leftSign: Byte = -1
         val leftExponent: Short = 0
         val leftMagnitude: Long = 10
 
-        // -11
+        // 0
+        val rightSign: Byte = 0
+        val rightExponent: Short = 0
+        val rightMagnitude: Long = 0
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"-1\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares two positives with exponents with diff less than 16 (left exponent is bigger)`() {
+        // 2E+10
+        val leftSign: Byte = 1
+        val leftExponent: Short = 10
+        val leftMagnitude: Long = 2
+
+        // 1.2345E+10
+        val rightSign: Byte = 1
+        val rightExponent: Short = 5
+        val rightMagnitude: Long = 123456
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"1\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares two positives with exponents with diff less than 16 (right exponent is bigger)`() {
+        // 1.2345E+10
+        val leftSign: Byte = 1
+        val leftExponent: Short = 5
+        val leftMagnitude: Long = 123456
+
+        // 2E+10
+        val rightSign: Byte = 1
+        val rightExponent: Short = 10
+        val rightMagnitude: Long = 2
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"-1\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares two negatives with exponents with diff less than 16 (left exponent is bigger)`() {
+        // -2E+10
+        val leftSign: Byte = -1
+        val leftExponent: Short = 10
+        val leftMagnitude: Long = 2
+
+        // -1.2345E+10
         val rightSign: Byte = -1
+        val rightExponent: Short = 5
+        val rightMagnitude: Long = 123456
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"-1\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares two negatives with exponents with diff less than 16 (right exponent is bigger)`() {
+        // -1.2345E+10
+        val leftSign: Byte = -1
+        val leftExponent: Short = 5
+        val leftMagnitude: Long = 123456
+
+        // -2E+10
+        val rightSign: Byte = -1
+        val rightExponent: Short = 10
+        val rightMagnitude: Long = 2
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"1\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares two positives with exponents with diff not less than 16 (left exponent is bigger)`() {
+        // 2E+10
+        val leftSign: Byte = 1
+        val leftExponent: Short = 21
+        val leftMagnitude: Long = 2
+
+        // 1.2345E+10
+        val rightSign: Byte = 1
+        val rightExponent: Short = 5
+        val rightMagnitude: Long = 123456
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"1\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares two positives with exponents with diff not less than 16 (right exponent is bigger)`() {
+        // 1.2345E+10
+        val leftSign: Byte = 1
+        val leftExponent: Short = 5
+        val leftMagnitude: Long = 123456
+
+        // 2E+10
+        val rightSign: Byte = 1
+        val rightExponent: Short = 21
+        val rightMagnitude: Long = 2
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"-1\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares two negatives with exponents with diff not less than 16 (left exponent is bigger)`() {
+        // -2E+21
+        val leftSign: Byte = -1
+        val leftExponent: Short = 21
+        val leftMagnitude: Long = 2
+
+        // -1.2345E+10
+        val rightSign: Byte = -1
+        val rightExponent: Short = 5
+        val rightMagnitude: Long = 123456
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"-1\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares two negatives with exponents with diff not less than 16 (right exponent is bigger)`() {
+        // -1.2345E+10
+        val leftSign: Byte = -1
+        val leftExponent: Short = 5
+        val leftMagnitude: Long = 123456
+
+        // -2E+21
+        val rightSign: Byte = -1
+        val rightExponent: Short = 21
+        val rightMagnitude: Long = 2
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"1\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares two positives (left is greater)`() {
+        // 11
+        val leftSign: Byte = 1
+        val leftExponent: Short = 0
+        val leftMagnitude: Long = 11
+
+        // 10
+        val rightSign: Byte = 1
+        val rightExponent: Short = 0
+        val rightMagnitude: Long = 10
+
+        val input = "{\"left\": ${toZincString(leftSign, leftExponent, leftMagnitude)}" +
+            ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
+
+        val output = zincZKService.prove(input.toByteArray())
+        zincZKService.verify(output, "\"1\"".toByteArray())
+    }
+
+    @Test
+    fun `zinc compares two positives (right is greater)`() {
+        // 10
+        val leftSign: Byte = 1
+        val leftExponent: Short = 0
+        val leftMagnitude: Long = 10
+
+        // 11
+        val rightSign: Byte = 1
         val rightExponent: Short = 0
         val rightMagnitude: Long = 11
 
@@ -41,7 +276,7 @@ class DoubleComparisonTest {
             ",\"right\": ${toZincString(rightSign, rightExponent, rightMagnitude)}}"
 
         val output = zincZKService.prove(input.toByteArray())
-        zincZKService.verify(output, "\"1\"".toByteArray())
+        zincZKService.verify(output, "\"-1\"".toByteArray())
     }
 
     private fun toZincString(sign: Byte, exponent: Short, magnitude: Long) =
