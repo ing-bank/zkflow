63cde07a8adf026c9b8da346b9118ee01607a8cc
Viktor Sergeev <viktor.sergeev@ing.com>
2021-02-04 17:24:43 +0100

    Comment structures

--- notary/src/test/kotlin/com/ing/zknotary/common/input/util.kt
+++ notary/src/test/kotlin/com/ing/zknotary/common/input/util.kt
@@ -1,7 +1,7 @@
 package com.ing.zknotary.common.input
 
 fun toAmountJSON(quantity: Long, displayTokenSizeJSON: String, tokenHash: ByteArray) =
-    "{\"quantity\": \"$quantity\", \"display_token_size\": $displayTokenSizeJSON, \"token_hash\": ${tokenHash.toPrettyString()}}"
+    "{\"quantity\": \"$quantity\", \"display_token_size\": $displayTokenSizeJSON, \"token_name_hash\": ${tokenHash.toPrettyString()}}"
 
 fun toBigDecimalJSON(sign: Byte, integer: ByteArray, fraction: ByteArray) =
     "{\"sign\": \"$sign\", \"integer\": [${integer.joinToString { "\"${it}\"" }}], \"fraction\": ${fraction.toPrettyString()}}"
