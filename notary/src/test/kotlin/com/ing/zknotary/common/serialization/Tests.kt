fe76ed6ca87a946113452a13f1eb2593a0b88dc2
Victor Ermolaev <victorermolaev@gmail.com>
2021-03-27 22:19:06 +0100

    Add convenience tests.

---
+++ notary/src/test/kotlin/com/ing/zknotary/common/serialization/Tests.kt
@@ -0,0 +1,33 @@
+package com.ing.zknotary.common.serialization
+
+import com.ing.serialization.bfl.deserialize
+import com.ing.serialization.bfl.serialize
+import com.ing.zknotary.common.serialization.bfl.corda.CordaSerializers
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlinx.serialization.ExperimentalSerializationApi
+
+/**
+ * Test if value survives serialization/deserialization.
+ */
+@ExperimentalSerializationApi
+inline fun <reified T : Any> roundTrip(value: T) {
+    val serialization = serialize(value, CordaSerializers)
+    val deserialization = deserialize<T>(serialization, CordaSerializers)
+
+    deserialization shouldBe value
+}
+
+/**
+ * Test if serializations of different instances have the same size.
+ */
+@ExperimentalSerializationApi
+inline fun <reified T : Any> sameSize(value1: T, value2: T) {
+    value1 shouldNotBe value2
+
+    val serialization1 = serialize(value1, CordaSerializers)
+    val serialization2 = serialize(value2, CordaSerializers)
+
+    serialization1 shouldNotBe serialization2
+    serialization1.size shouldBe serialization2.size
+}
