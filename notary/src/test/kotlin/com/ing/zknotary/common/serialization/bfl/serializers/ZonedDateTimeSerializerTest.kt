989ac0775e734b8bac89c5adeed507b4e74fee43
Peterpaul Klein Haneveld <peterpaul@tppp.home>
2021-05-04 13:59:48 +0200

    Use parameterized tests

--- notary/src/test/kotlin/com/ing/zknotary/common/serialization/bfl/serializers/ZonedDateTimeSerializerTest.kt
+++ notary/src/test/kotlin/com/ing/zknotary/common/serialization/bfl/serializers/ZonedDateTimeSerializerTest.kt
@@ -5,33 +5,21 @@ import com.ing.zknotary.testing.assertSameSize
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import java.time.Year
 import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.ZonedDateTime
-import com.ing.serialization.bfl.api.reified.debugSerialize as debugSerializeInlined
 
 class ZonedDateTimeSerializerTest {
     @Serializable
     data class Data(val date: @Contextual ZonedDateTime)
 
-    @Test
-    fun `ZonedDateTime should be serialized successfully`() {
-        val data = Data(ZonedDateTime.now())
-        println(debugSerializeInlined(data, serializersModule = CordaSerializers).second)
-    }
-
-    @Test
-    fun `ZonedDateTime should be the same after serialization and deserialization`() {
-        listOf(
-            Data(ZonedDateTime.now()),
-            Data(ZonedDateTime.now(ZoneId.of("America/Argentina/ComodRivadavia"))),
-            Data(ZonedDateTime.now(ZoneOffset.ofTotalSeconds(42))),
-            Data(ZonedDateTime.of(Year.MAX_VALUE, 1, 1, 0, 0, 0, 0, ZoneId.of("GMT"))),
-            Data(ZonedDateTime.of(Year.MIN_VALUE, 1, 1, 0, 0, 0, 0, ZoneId.of("GMT"))),
-        ).forEach { data ->
-            assertRoundTripSucceeds(data)
-        }
+    @ParameterizedTest
+    @MethodSource("testData")
+    fun `ZonedDateTime should be the same after serialization and deserialization`(data: Data) {
+        assertRoundTripSucceeds(data)
     }
 
     @Test
@@ -40,5 +28,16 @@ class ZonedDateTimeSerializerTest {
         val data2 = Data(ZonedDateTime.now(ZoneOffset.ofTotalSeconds(42)))
 
         assertSameSize(data1, data2)
+    }
+
+    companion object {
+        @JvmStatic
+        fun testData() = listOf(
+            Data(ZonedDateTime.now()),
+            Data(ZonedDateTime.now(ZoneId.of("America/Argentina/ComodRivadavia"))),
+            Data(ZonedDateTime.now(ZoneOffset.ofTotalSeconds(42))),
+            Data(ZonedDateTime.of(Year.MAX_VALUE, 1, 1, 0, 0, 0, 0, ZoneId.of("GMT"))),
+            Data(ZonedDateTime.of(Year.MIN_VALUE, 1, 1, 0, 0, 0, 0, ZoneId.of("GMT"))),
+        )
     }
 }
--- zinc-platform-sources/src/test/kotlin/zinc/types/DeserializeZonedDateTimeTest.kt
+++ zinc-platform-sources/src/test/kotlin/zinc/types/DeserializeZonedDateTimeTest.kt
@@ -4,7 +4,8 @@ import io.kotest.matchers.shouldBe
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import java.time.Year
 import java.time.ZoneId
 import java.time.ZoneOffset
@@ -15,18 +16,9 @@ import kotlin.time.ExperimentalTime
 class DeserializeZonedDateTimeTest {
     private val zincZKService = getZincZKService<DeserializeZonedDateTimeTest>()
 
-    @Test
-    fun `an Instance should be deserialized correctly`() {
-        listOf(
-            Data(ZonedDateTime.now()),
-            Data(ZonedDateTime.now(ZoneId.of("America/Argentina/ComodRivadavia"))),
-            Data(ZonedDateTime.now(ZoneOffset.ofTotalSeconds(42))),
-            Data(ZonedDateTime.of(Year.MAX_VALUE, 1, 1, 0, 0, 0, 0, ZoneId.of("GMT"))),
-            Data(ZonedDateTime.of(Year.MIN_VALUE, 1, 1, 0, 0, 0, 0, ZoneId.of("GMT")))
-        ).forEach(this::performDeserializationTest)
-    }
-
-    private fun performDeserializationTest(data: Data) {
+    @ParameterizedTest
+    @MethodSource("testData")
+    fun performDeserializationTest(data: Data) {
         val witness = toWitness(data)
 
         val expected = data.data.toZincJson()
@@ -39,7 +31,18 @@ class DeserializeZonedDateTimeTest {
     }
 
     @Serializable
-    private data class Data(
+    data class Data(
         val data: @Contextual ZonedDateTime
     )
+
+    companion object {
+        @JvmStatic
+        fun testData() = listOf(
+            Data(ZonedDateTime.now()),
+            Data(ZonedDateTime.now(ZoneId.of("America/Argentina/ComodRivadavia"))),
+            Data(ZonedDateTime.now(ZoneOffset.ofTotalSeconds(42))),
+            Data(ZonedDateTime.of(Year.MAX_VALUE, 1, 1, 0, 0, 0, 0, ZoneId.of("GMT"))),
+            Data(ZonedDateTime.of(Year.MIN_VALUE, 1, 1, 0, 0, 0, 0, ZoneId.of("GMT")))
+        )
+    }
 }
