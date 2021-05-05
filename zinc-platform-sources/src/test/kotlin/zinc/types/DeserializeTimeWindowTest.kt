124855c77133a70739a0790036483559605f1bdc
Victor Ermolaev <victorermolaev@gmail.com>
2021-05-05 15:46:12 +0200

    Implement comparison for Instant and inclusion relation to TimeWindow.

--- zinc-platform-sources/src/test/kotlin/zinc/types/DeserializeTimeWindowTest.kt
+++ zinc-platform-sources/src/test/kotlin/zinc/types/DeserializeTimeWindowTest.kt
@@ -5,7 +5,8 @@ import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import net.corda.core.contracts.TimeWindow
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import java.time.Duration
 import java.time.Instant
 import kotlin.time.ExperimentalTime
@@ -14,31 +15,39 @@ import kotlin.time.ExperimentalTime
 class DeserializeTimeWindowTest {
     private val zincZKService = getZincZKService<DeserializeTimeWindowTest>()
 
-    private val timeWindows: List<TimeWindow> = listOf(
+    private val timeData: List<TimeWindow> = listOf(
         TimeWindow.fromOnly(Instant.now()),
         TimeWindow.untilOnly(Instant.now()),
         TimeWindow.between(Instant.now(), Instant.now().plusSeconds(3600)),
         TimeWindow.fromStartAndDuration(Instant.now(), Duration.ofSeconds(3600)),
     )
 
-    @Test
-    fun `a TimeWindow should be deserialized correctly`() {
-        timeWindows.forEach {
-            val data = Data(it)
-            val witness = toWitness(data)
+    @ParameterizedTest
+    @MethodSource("testData")
+    fun `a TimeWindow should be deserialized correctly`(data: Data) {
+        val witness = toWitness(data)
 
-            val expected = data.data.toZincJson()
-            val actual = zincZKService.run(witness, "")
+        val expected = data.data.toZincJson()
+        val actual = zincZKService.run(witness, "")
 
-            val expectedJson = Json.parseToJsonElement(expected)
-            val actualJson = Json.parseToJsonElement(actual)
+        val expectedJson = Json.parseToJsonElement(expected)
+        val actualJson = Json.parseToJsonElement(actual)
 
-            actualJson shouldBe expectedJson
-        }
+        actualJson shouldBe expectedJson
     }
 
     @Serializable
-    private data class Data(
+    data class Data(
         val data: @Contextual TimeWindow
     )
+
+    companion object {
+        @JvmStatic
+        fun testData() = listOf(
+            TimeWindow.fromOnly(Instant.now()),
+            TimeWindow.untilOnly(Instant.now()),
+            TimeWindow.between(Instant.now(), Instant.now().plusSeconds(3600)),
+            TimeWindow.fromStartAndDuration(Instant.now(), Duration.ofSeconds(3600)),
+        ).map { Data(it) }
+    }
 }
