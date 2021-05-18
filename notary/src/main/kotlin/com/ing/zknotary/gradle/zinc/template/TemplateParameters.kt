26902dc544f3fdb95c33254295b77b50caa9e878
Peterpaul Klein Haneveld <peterpaul@tppp.home>
2021-05-18 15:56:24 +0200

    Refactor: extract replacements and targetFile to TemplateParameters

--- notary/src/main/kotlin/com/ing/zknotary/gradle/zinc/template/TemplateParameters.kt
+++ notary/src/main/kotlin/com/ing/zknotary/gradle/zinc/template/TemplateParameters.kt
@@ -16,6 +16,9 @@ sealed class TemplateParameters(
 ) {
     fun resolveAllConfigurations(): List<TemplateParameters> =
         dependencies.flatMap { it.resolveAllConfigurations() } + this
+
+    open fun getReplacements(): Map<String, String> = emptyMap()
+    open fun getTargetFilename(): String = templateFile
 }
 
 data class BigDecimalTemplateParameters(
@@ -28,7 +31,16 @@ data class BigDecimalTemplateParameters(
     /**
      * The name of the struct for this configuration.
      */
-    val typeName: String by lazy { typeNameOverride ?: BIG_DECIMAL + postFix() }
+    internal val typeName: String by lazy { typeNameOverride ?: BIG_DECIMAL + postFix() }
+
+    override fun getReplacements(): Map<String, String> = mapOf(
+        "TYPE_NAME" to typeName,
+        "CONSTANT_PREFIX" to typeName.camelToSnakeCase().toUpperCase(),
+        "INTEGER_SIZE_PLACEHOLDER" to integerSize.toString(),
+        "FRACTION_SIZE_PLACEHOLDER" to fractionSize.toString(),
+    )
+
+    override fun getTargetFilename() = "${typeName.camelToSnakeCase()}.zn"
 
     companion object {
         const val BIG_DECIMAL = "BigDecimal"
@@ -45,7 +57,18 @@ data class AmountTemplateParameters(
     /**
      * The name of the struct for this configuration.
      */
-    val typeName: String by lazy { typeNameOverride ?: AMOUNT + postFix() + tokenDisplaySize.typeName }
+    private val typeName: String by lazy { typeNameOverride ?: AMOUNT + postFix() + tokenDisplaySize.typeName }
+
+    override fun getReplacements() = mapOf(
+        "TYPE_NAME" to typeName,
+        "CONSTANT_PREFIX" to typeName.camelToSnakeCase().toUpperCase(),
+        "TOKEN_SIZE_PLACEHOLDER" to tokenSize.toString(),
+        "BD_TYPE_NAME" to tokenDisplaySize.typeName,
+        "BD_CONSTANT_PREFIX" to tokenDisplaySize.typeName.camelToSnakeCase().toUpperCase(),
+        "BD_MODULE_NAME" to tokenDisplaySize.typeName.camelToSnakeCase(),
+    )
+
+    override fun getTargetFilename() = "${typeName.camelToSnakeCase()}.zn"
 
     companion object {
         const val AMOUNT = "Amount"
@@ -54,11 +77,25 @@ data class AmountTemplateParameters(
 
 data class StringTemplateParameters(
     val stringSize: Short
-) : TemplateParameters("string.zn", emptyList())
+) : TemplateParameters("string.zn", emptyList()) {
+    override fun getReplacements() = mapOf(
+        "TYPE_NAME" to "String_$stringSize",
+        "CONSTANT_PREFIX" to "STRING_$stringSize",
+        "STRING_SIZE_PLACEHOLDER" to stringSize.toString(),
+    )
+
+    override fun getTargetFilename() = "string_$stringSize.zn"
+}
 
 data class ByteArrayTemplateParameters(
     val arraySize: Int
-) : TemplateParameters("byte_array.zn", emptyList())
+) : TemplateParameters("byte_array.zn", emptyList()) {
+    override fun getReplacements() = mapOf(
+        "ARRAY_LENGTH" to "$arraySize",
+    )
+
+    override fun getTargetFilename() = "byte_array_$arraySize.zn"
+}
 
 data class PublicKeyTemplateParameters(
     override val templateFile: String,
@@ -71,6 +108,19 @@ data class PublicKeyTemplateParameters(
         ByteArrayTemplateParameters(encodedSize),
     )
 ) {
+    private val snakeCaseType by lazy {
+        typeName.camelToSnakeCase().replace("public_key", "_public_key")
+    }
+
+    override fun getReplacements() = mapOf(
+        "TYPE_NAME" to typeName,
+        "CONST_PREFIX" to snakeCaseType.toUpperCase(),
+        "ENCODED_SIZE" to "$encodedSize",
+        "TYPE_SIZE" to "1",
+    )
+
+    override fun getTargetFilename() = "$snakeCaseType.zn"
+
     companion object {
         private const val TEMPLATE = "public_key.zn"
         private const val TEMPLATE_WITH_SCHEME_ID = "public_key_with_scheme_id.zn"
@@ -88,6 +138,21 @@ data class AbstractPartyTemplateParameters(val pkTemplateParameters: PublicKeyTe
     "abstract_party.zn",
     listOf(pkTemplateParameters)
 ) {
+    private val pkTypeName by lazy { pkTemplateParameters.typeName }
+    private val algName by lazy { pkTypeName.removeSuffix("PublicKey") }
+    private val pkSnakeCaseType by lazy { pkTypeName.camelToSnakeCase().replace("public_key", "_public_key") }
+    private val typeName by lazy { "AbstractParty$algName" }
+
+    override fun getReplacements() = mapOf(
+        "TYPE_NAME" to typeName,
+        "CONSTANT_PREFIX" to typeName.camelToSnakeCase().toUpperCase(),
+        "PK_TYPE_NAME" to pkTypeName,
+        "PK_CONSTANT_PREFIX" to pkSnakeCaseType.toUpperCase(),
+        "PK_MODULE_NAME" to pkSnakeCaseType,
+    )
+
+    override fun getTargetFilename() = "${typeName.camelToSnakeCase()}.zn"
+
     companion object {
         val all = PublicKeyTemplateParameters.all.map {
             AbstractPartyTemplateParameters(it)
@@ -99,6 +164,21 @@ data class AnonymousPartyTemplateParameters(val pkTemplateParameters: PublicKeyT
     "anonymous_party.zn",
     listOf(pkTemplateParameters)
 ) {
+    private val pkTypeName by lazy { pkTemplateParameters.typeName }
+    private val algName by lazy { pkTypeName.removeSuffix("PublicKey") }
+    private val pkSnakeCaseType by lazy { pkTypeName.camelToSnakeCase().replace("public_key", "_public_key") }
+    private val typeName by lazy { "AnonymousParty$algName" }
+
+    override fun getReplacements() = mapOf(
+        "TYPE_NAME" to typeName,
+        "CONSTANT_PREFIX" to typeName.camelToSnakeCase().toUpperCase(),
+        "PK_TYPE_NAME" to pkTypeName,
+        "PK_CONSTANT_PREFIX" to pkSnakeCaseType.toUpperCase(),
+        "PK_MODULE_NAME" to pkSnakeCaseType,
+    )
+
+    override fun getTargetFilename() = "${typeName.camelToSnakeCase()}.zn"
+
     companion object {
         val all = PublicKeyTemplateParameters.all.map {
             AnonymousPartyTemplateParameters(it)
@@ -110,6 +190,21 @@ data class PartyTemplateParameters(val pkTemplateParameters: PublicKeyTemplatePa
     "party.zn",
     listOf(pkTemplateParameters)
 ) {
+    private val pkTypeName by lazy { pkTemplateParameters.typeName }
+    private val algName by lazy { pkTypeName.removeSuffix("PublicKey") }
+    private val pkSnakeCaseType by lazy { pkTypeName.camelToSnakeCase().replace("public_key", "_public_key") }
+    private val typeName by lazy { "Party$algName" }
+
+    override fun getReplacements() = mapOf(
+        "TYPE_NAME" to typeName,
+        "CONSTANT_PREFIX" to typeName.camelToSnakeCase().toUpperCase(),
+        "PK_TYPE_NAME" to pkTypeName,
+        "PK_CONSTANT_PREFIX" to pkSnakeCaseType.toUpperCase(),
+        "PK_MODULE_NAME" to pkSnakeCaseType,
+    )
+
+    override fun getTargetFilename() = "${typeName.camelToSnakeCase()}.zn"
+
     companion object {
         val all = PublicKeyTemplateParameters.all.map {
             PartyTemplateParameters(it)
@@ -120,27 +215,47 @@ data class PartyTemplateParameters(val pkTemplateParameters: PublicKeyTemplatePa
 object UniqueIdentifierTemplateParameters : TemplateParameters(
     "unique_identifier.zn",
     listOf(StringTemplateParameters(UniqueIdentifierSurrogate.EXTERNAL_ID_LENGTH.toShort()))
-)
+) {
+    override fun getReplacements() = mapOf(
+        "EXTERNAL_ID_STRING_SIZE" to UniqueIdentifierSurrogate.EXTERNAL_ID_LENGTH.toString(),
+    )
+}
 
 object LinearPointerTemplateParameters : TemplateParameters(
     "linear_pointer.zn",
     listOf(StringTemplateParameters(LinearPointerSurrogate.MAX_CLASS_NAME_SIZE.toShort()))
-)
+) {
+    override fun getReplacements() = mapOf(
+        "CLASS_NAME_STRING_SIZE" to LinearPointerSurrogate.MAX_CLASS_NAME_SIZE.toString(),
+    )
+}
 
 object X500PrincipalTemplateParameters : TemplateParameters(
     "x500_principal.zn",
     listOf(StringTemplateParameters(X500PrincipalSurrogate.PRINCIPAL_SIZE.toShort()))
-)
+) {
+    override fun getReplacements() = mapOf(
+        "PRINCIPAL_NAME_STRING_SIZE" to X500PrincipalSurrogate.PRINCIPAL_SIZE.toString(),
+    )
+}
 
 object CurrencyTemplateParameters : TemplateParameters(
     "currency.zn",
     listOf(StringTemplateParameters(CurrencySurrogate.CURRENCY_SIZE.toShort()))
-)
+) {
+    override fun getReplacements() = mapOf(
+        "CURRENCY_CODE_STRING_SIZE" to CurrencySurrogate.CURRENCY_SIZE.toString(),
+    )
+}
 
 object SecureHashTemplateParameters : TemplateParameters(
     "secure_hash.zn",
     listOf(ByteArrayTemplateParameters(SecureHashSurrogate.BYTES_SIZE))
-)
+) {
+    override fun getReplacements() = mapOf(
+        "BYTES_SIZE" to SecureHashSurrogate.BYTES_SIZE.toString(),
+    )
+}
 
 private val camelRegex = "(?<=[a-z])[A-Z]".toRegex()
 
