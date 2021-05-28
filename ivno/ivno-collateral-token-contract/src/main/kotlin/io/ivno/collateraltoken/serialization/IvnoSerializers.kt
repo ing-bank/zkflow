def263e28fdfaf4a5c3e16357f08416b99d3d7a4
SereV94 <vasileios.serentellos@ing.com>
2021-05-28 11:46:51 +0200

    Add serialization support for Role

--- ivno/ivno-collateral-token-contract/src/main/kotlin/io/ivno/collateraltoken/serialization/IvnoSerializers.kt
+++ ivno/ivno-collateral-token-contract/src/main/kotlin/io/ivno/collateraltoken/serialization/IvnoSerializers.kt
@@ -15,7 +15,7 @@ object IvnoSerializers {
         loggerFor<IvnoSerializers>().info("Registering Ivno serializers")
         SerializersModuleRegistry.register(SerializersModule {
             contextual(BigDecimalAmountSerializer(LinearPointerSerializer))
-            // Add here
+            contextual(RoleSerializer)
         })
 
         ContractStateSerializerMap.register(Deposit::class, 1, Deposit.serializer())
