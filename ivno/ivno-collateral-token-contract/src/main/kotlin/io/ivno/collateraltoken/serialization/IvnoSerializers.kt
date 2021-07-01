4c40e0ba3881e283dc940bd71871ff314b110dcc
Victor Ermolaev <victorermolaev@gmail.com>
2021-07-01 10:06:30 +0200

    Add/register a serializer for Membership.Issue and for ClaimSerializer for Int inner types.

--- ivno/ivno-collateral-token-contract/src/main/kotlin/io/ivno/collateraltoken/serialization/IvnoSerializers.kt
+++ ivno/ivno-collateral-token-contract/src/main/kotlin/io/ivno/collateraltoken/serialization/IvnoSerializers.kt
@@ -3,10 +3,25 @@ package io.ivno.collateraltoken.serialization
 import com.ing.zknotary.common.serialization.bfl.CommandDataSerializerMap
 import com.ing.zknotary.common.serialization.bfl.ContractStateSerializerMap
 import com.ing.zknotary.common.serialization.bfl.SerializersModuleRegistry
+import com.ing.zknotary.common.serialization.bfl.serializers.StateRefSerializer
 import io.ivno.collateraltoken.contract.Deposit
 import io.ivno.collateraltoken.contract.DepositContract
+import io.onixlabs.corda.bnms.contract.membership.Membership
+import io.onixlabs.corda.bnms.contract.membership.MembershipContract
+import io.onixlabs.corda.identityframework.contract.AbstractClaim
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.PolymorphicSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.contextual
+import kotlinx.serialization.modules.polymorphic
+import kotlinx.serialization.modules.subclass
+import net.corda.core.contracts.StateRef
 import net.corda.core.utilities.loggerFor
 
 object IvnoSerializers {
@@ -24,6 +39,16 @@ object IvnoSerializers {
         contextual(TokenTransactionSummaryStateSerializer)
         contextual(AttestationSerializer)
         contextual(MembershipAttestationSerializer)
+
+        polymorphic(AbstractClaim::class) {
+            subclass(ClaimSerializer(PolymorphicSerializer(Any::class)))
+        }
+
+        polymorphic(Any::class) {
+            subclass(Int::class, MyIntSerializer)
+        }
+
+        contextual(MembershipContractIssueSerializer)
     }
 
     init {
@@ -31,6 +56,17 @@ object IvnoSerializers {
         SerializersModuleRegistry.register(serializersModule)
 
         ContractStateSerializerMap.register(Deposit::class, 1, Deposit.serializer())
+
+        // TODO This only works with Ints as inner types for identity and settings.
+        ContractStateSerializerMap.register(Membership::class, 2, MembershipWithIntSerializer)
+
         CommandDataSerializerMap.register(DepositContract.Request::class, 3, DepositContract.Request.serializer())
+        CommandDataSerializerMap.register(MembershipContract.Issue::class, 4, MembershipContractIssueSerializer)
     }
+}
+
+object MembershipContractIssueSerializer: KSerializer<MembershipContract.Issue> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("dummy", PrimitiveKind.BYTE)
+    override fun deserialize(decoder: Decoder): MembershipContract.Issue = MembershipContract.Issue
+    override fun serialize(encoder: Encoder, value: MembershipContract.Issue) {}
 }
