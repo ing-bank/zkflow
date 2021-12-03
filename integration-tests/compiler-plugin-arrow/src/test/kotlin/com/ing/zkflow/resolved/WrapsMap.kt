17dd00f49868322d04d64e79dd1aca354bc51a29
Victor Ermolaev <victorermolaev@gmail.com>
2021-12-03 10:51:34 +0100

    Add an integration test for a map for the arrow compiler plugin.

---
+++ integration-tests/compiler-plugin-arrow/src/test/kotlin/com/ing/zkflow/resolved/WrapsMap.kt
@@ -0,0 +1,42 @@
+package com.ing.zkflow.resolved
+
+import com.ing.zkflow.serialization.SerializerTest
+import com.ing.zkflow.serialization.engine.SerdeEngine
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+@Suppress("ClassName")
+@kotlinx.serialization.Serializable
+data class WrapsMap(
+    @kotlinx.serialization.Serializable(with = Map_0::class)
+    val map: Map<Int, String?> = mapOf(1 to "test", 2 to null)
+) {
+    object Map_0 : com.ing.zkflow.serialization.serializer.FixedLengthMapSerializer<Int, String?>(
+        5, Map_0_A_0, Map_0_B_0
+    )
+    object Map_0_A_0 : com.ing.zkflow.serialization.serializer.WrappedKSerializerWithDefault<Int>(com.ing.zkflow.serialization.serializer.IntSerializer)
+    object Map_0_B_0 : com.ing.zkflow.serialization.serializer.NullableSerializer<String>(Map_0_B_1)
+    object Map_0_B_1 : com.ing.zkflow.serialization.serializer.string.FixedLengthASCIIStringSerializer(10)
+}
+
+class WrapsMapTest : SerializerTest {
+    @ParameterizedTest
+    @MethodSource("engines")
+    fun `WrapsBigDecimal make a round trip`(engine: SerdeEngine) {
+        engine.assertRoundTrip(
+            com.ing.zkflow.annotated.WrapsMap.serializer(),
+            com.ing.zkflow.annotated.WrapsMap(), true
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("engines")
+    fun `WrapsBigDecimal generated and manual serializations must coincide`(engine: SerdeEngine) {
+        engine.serialize(
+            com.ing.zkflow.annotated.WrapsBigDecimal.serializer(),
+            com.ing.zkflow.annotated.WrapsBigDecimal()
+        ) shouldBe
+            engine.serialize(WrapsBigDecimal.serializer(), WrapsBigDecimal())
+    }
+}
