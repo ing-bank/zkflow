c905b9b76b25bf116370848e1d9911ee7781463e
Victor Ermolaev <victorermolaev@gmail.com>
2021-12-03 10:50:31 +0100

    Add units specification to BinaryFixedLengthScheme.

--- test-utils/src/main/kotlin/com/ing/zkflow/serialization/engine/BFLEngine.kt
+++ test-utils/src/main/kotlin/com/ing/zkflow/serialization/engine/BFLEngine.kt
@@ -6,16 +6,15 @@ import com.ing.zkflow.serialization.scheme.ByteBinaryFixedLengthScheme
 import kotlinx.serialization.KSerializer
 
 public open class BFLEngine(private val scheme: BinaryFixedLengthScheme) : SerdeEngine {
-
     override fun <T> serialize(strategy: KSerializer<T>, value: T, shouldPrint: Boolean): ByteArray {
         if (shouldPrint) { println("Serializing:\n$value") }
         return scheme.encodeToBinary(strategy, value).also {
-            if (shouldPrint) println("Serialized:\n${it.joinToString(separator = ",")}")
+            if (shouldPrint) println("Serialized (${scheme.unit}):\n${it.joinToString(separator = ",")}")
         }
     }
 
     override fun <T> deserialize(strategy: KSerializer<T>, data: ByteArray, shouldPrint: Boolean): T {
-        if (shouldPrint) { println("Deserializing:\n${data.joinToString(separator = ",")}") }
+        if (shouldPrint) { println("Deserializing (${scheme.unit}):\n${data.joinToString(separator = ",")}") }
 
         return scheme.decodeFromBinary(strategy, data).also {
             if (shouldPrint) println("Deserialized:\n$it")
