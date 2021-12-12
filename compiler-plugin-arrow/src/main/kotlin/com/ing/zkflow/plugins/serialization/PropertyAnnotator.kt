41619c42079ee7124a5100fe8828c0dee945c89e
Victor Ermolaev <victorermolaev@gmail.com>
2021-12-12 13:55:41 +0100

    Stop generating `@Transient` annotation for fields with no backing fields.

--- compiler-plugin-arrow/src/main/kotlin/com/ing/zkflow/plugins/serialization/PropertyAnnotator.kt
+++ compiler-plugin-arrow/src/main/kotlin/com/ing/zkflow/plugins/serialization/PropertyAnnotator.kt
@@ -8,7 +8,10 @@ import arrow.meta.quotes.property
 import com.ing.zkflow.SerdeLogger
 import com.ing.zkflow.ZKP
 import kotlinx.serialization.Transient
+import org.jetbrains.kotlin.psi.KtConstantExpression
+import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 import org.jetbrains.kotlin.psi.psiUtil.containingClass
 import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
 import org.jetbrains.kotlin.util.isOrdinaryClass
@@ -23,7 +26,7 @@ val Meta.PropertyAnnotator: CliPlugin
         meta(
             property(this, match = {
                 element.verifyAnnotationCorrectness()
-            }) { (ktProperty, descriptor) ->
+            }) { (ktProperty, _) ->
                 // Arrow has troubles with comments and doc strings, remove them altogether.
                 val allComments = "((['\"])(?:(?!\\2|\\\\).|\\\\.)*\\2)|\\/\\/[^\\n]*|\\/\\*(?:[^*]|\\*(?!\\/))*\\*\\/".toRegex()
                 val propertyClearDeclaration = ktProperty.text
@@ -51,12 +54,42 @@ private fun KtProperty.verifyAnnotationCorrectness(): Boolean {
     SerdeLogger.log(PROCESSING_UNIT)
     SerdeLogger.logShort("Considering:\n$text")
 
-    val applicability = containingClassOrObject?.isOrdinaryClass ?: false &&
-        containingClass()?.let {
-            it.hasAnnotation<ZKP>() && it.isCorrectClassTypeForZKPAnnotation()
-        } ?: false
+    val applicability = (containingClassOrObject?.isOrdinaryClass ?: false) &&
+        (containingClass()?.let { it.hasAnnotation<ZKP>() && it.isCorrectClassTypeForZKPAnnotation() } ?: false) &&
+        hasBackingField()
 
     SerdeLogger.log("(PROP) ${if (applicability) "SHALL" else "WILL NOT"} process")
 
     return applicability
+}
+
+private fun KtProperty.hasBackingField(): Boolean {
+    val getsField = (getter?.bodyExpression ?: getter?.bodyBlockExpression)?.isNotConst() ?: true
+    val setsField = (setter?.bodyExpression ?: setter?.bodyBlockExpression)?.isNotConst() ?: true
+
+    return getsField && setsField
+}
+
+/**
+ * A sub-optimal way to deduce whether expression is a constant.
+ * For proper analysis, consult with
+ * https://github.com/JetBrains/intellij-community/pull/1678/files#diff-18106261245e00b0e9cf958fc4b077006359069ad528075566509418d11d9e57R154
+ * However, that analysis uses BindingContext which is
+ * (it seems to me, will be happy if someone proves me wrong)
+ * inaccessible here.
+ */
+private fun KtExpression.isNotConst(): Boolean {
+    SerdeLogger.log(text)
+    return when (this) {
+        is KtConstantExpression -> false
+        is KtStringTemplateExpression -> this.hasInterpolation()
+        else -> {
+            val fieldReferences = "^[\\s\\S&&[^/]]*field.*\$".toRegex()
+            // E.g., on the second one will match
+            // |  // field = 2
+            // |    ({field = 2})  //
+            // | x = 2 // field = 2
+            this.text.contains(fieldReferences)
+        }
+    }
 }
