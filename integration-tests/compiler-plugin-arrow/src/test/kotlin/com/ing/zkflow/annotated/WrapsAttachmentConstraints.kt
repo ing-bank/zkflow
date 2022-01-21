ca4813e22e769a7342aa032603bbee111bfce2e5
Victor Ermolaev <victorermolaev@gmail.com>
2022-01-21 13:28:13 +0100

    Add ZKP support for AttachmentConstraint.

---
+++ integration-tests/compiler-plugin-arrow/src/test/kotlin/com/ing/zkflow/annotated/WrapsAttachmentConstraints.kt
@@ -0,0 +1,28 @@
+@file:Suppress("DEPRECATION")
+
+package com.ing.zkflow.annotated
+
+import com.ing.zkflow.annotations.ZKP
+import com.ing.zkflow.annotations.corda.EdDSA
+import com.ing.zkflow.annotations.corda.Sha256
+import com.ing.zkflow.serialization.serializer.corda.PublicKeySerializer
+import net.corda.core.contracts.AlwaysAcceptAttachmentConstraint
+import net.corda.core.contracts.AutomaticHashConstraint
+import net.corda.core.contracts.AutomaticPlaceholderConstraint
+import net.corda.core.contracts.HashAttachmentConstraint
+import net.corda.core.contracts.SignatureAttachmentConstraint
+import net.corda.core.contracts.WhitelistedByZoneAttachmentConstraint
+import net.corda.core.crypto.Crypto
+import net.corda.core.crypto.SecureHash
+
+@ZKP
+data class WrapsAttachmentConstraints(
+    val alwaysAcceptAttachmentConstraint: AlwaysAcceptAttachmentConstraint = AlwaysAcceptAttachmentConstraint,
+    val hashAttachmentConstraint: @Sha256 HashAttachmentConstraint = HashAttachmentConstraint(SecureHash.zeroHash),
+    val whitelistedByZoneAttachmentConstraint: WhitelistedByZoneAttachmentConstraint = WhitelistedByZoneAttachmentConstraint,
+    val automaticHashConstraint: AutomaticHashConstraint = AutomaticHashConstraint,
+    val automaticPlaceholderConstraint: AutomaticPlaceholderConstraint = AutomaticPlaceholderConstraint,
+    val signatureAttachmentConstraint: @EdDSA SignatureAttachmentConstraint = SignatureAttachmentConstraint(
+        PublicKeySerializer.fixedPublicKey(Crypto.EDDSA_ED25519_SHA512)
+    ),
+)
---
+++ integration-tests/compiler-plugin-arrow/src/test/kotlin/com/ing/zkflow/resolved/WrapsAtachmentContraints.kt
@@ -0,0 +1,68 @@
+@file:Suppress("DEPRECATION")
+
+package com.ing.zkflow.resolved
+
+import com.ing.zkflow.serialization.SerializerTest
+import com.ing.zkflow.serialization.engine.SerdeEngine
+import com.ing.zkflow.serialization.serializer.WrappedKSerializerWithDefault
+import com.ing.zkflow.serialization.serializer.corda.AlwaysAcceptAttachmentConstraintSerializer
+import com.ing.zkflow.serialization.serializer.corda.AutomaticHashConstraintSerializer
+import com.ing.zkflow.serialization.serializer.corda.AutomaticPlaceholderConstraintSerializer
+import com.ing.zkflow.serialization.serializer.corda.HashAttachmentConstraintSerializer
+import com.ing.zkflow.serialization.serializer.corda.PublicKeySerializer
+import com.ing.zkflow.serialization.serializer.corda.SignatureAttachmentConstraintSerializer
+import com.ing.zkflow.serialization.serializer.corda.WhitelistedByZoneAttachmentConstraintSerializer
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.Serializable
+import net.corda.core.contracts.AlwaysAcceptAttachmentConstraint
+import net.corda.core.contracts.AutomaticHashConstraint
+import net.corda.core.contracts.AutomaticPlaceholderConstraint
+import net.corda.core.contracts.HashAttachmentConstraint
+import net.corda.core.contracts.SignatureAttachmentConstraint
+import net.corda.core.contracts.WhitelistedByZoneAttachmentConstraint
+import net.corda.core.crypto.Crypto
+import net.corda.core.crypto.SecureHash
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+@Suppress("ClassName")
+@Serializable
+data class WrapsAttachmentConstraints(
+    @Serializable(with = AlwaysAcceptAttachmentConstraint_0::class) val alwaysAcceptAttachmentConstraint: @Contextual AlwaysAcceptAttachmentConstraint = AlwaysAcceptAttachmentConstraint,
+    @Serializable(with = HashAttachmentConstraint_0::class) val hashAttachmentConstraint: @Contextual HashAttachmentConstraint = HashAttachmentConstraint(SecureHash.zeroHash),
+    @Serializable(with = WhitelistedByZoneAttachmentConstraint_0::class) val whitelistedByZoneAttachmentConstraint: @Contextual WhitelistedByZoneAttachmentConstraint = WhitelistedByZoneAttachmentConstraint,
+    @Serializable(with = AutomaticHashConstraint_0::class) val automaticHashConstraint: @Contextual AutomaticHashConstraint = AutomaticHashConstraint,
+    @Serializable(with = AutomaticPlaceholderConstraint_0::class) val automaticPlaceholderConstraint: @Contextual AutomaticPlaceholderConstraint = AutomaticPlaceholderConstraint,
+    @Serializable(with = SignatureAttachmentConstraint_0::class) val signatureAttachmentConstraint: @Contextual SignatureAttachmentConstraint = SignatureAttachmentConstraint(
+        PublicKeySerializer.fixedPublicKey(Crypto.EDDSA_ED25519_SHA512)
+    ),
+) {
+    object AlwaysAcceptAttachmentConstraint_0 : WrappedKSerializerWithDefault<AlwaysAcceptAttachmentConstraint>(AlwaysAcceptAttachmentConstraintSerializer)
+    object HashAttachmentConstraint_0 : HashAttachmentConstraintSerializer("Sha256", 32)
+    object WhitelistedByZoneAttachmentConstraint_0 : WrappedKSerializerWithDefault<WhitelistedByZoneAttachmentConstraint>(WhitelistedByZoneAttachmentConstraintSerializer)
+    object AutomaticHashConstraint_0 : WrappedKSerializerWithDefault<AutomaticHashConstraint>(AutomaticHashConstraintSerializer)
+    object AutomaticPlaceholderConstraint_0 : WrappedKSerializerWithDefault<AutomaticPlaceholderConstraint>(AutomaticPlaceholderConstraintSerializer)
+    object SignatureAttachmentConstraint_0 : SignatureAttachmentConstraintSerializer(4)
+}
+
+class WrapsAttachmentConstraintsTest : SerializerTest {
+    @ParameterizedTest
+    @MethodSource("engines")
+    fun `WrapsBasic make a round trip`(engine: SerdeEngine) {
+        engine.assertRoundTrip(
+            com.ing.zkflow.annotated.WrapsAttachmentConstraints.serializer(),
+            com.ing.zkflow.annotated.WrapsAttachmentConstraints()
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("engines")
+    fun `WrapsBasic generated and manual serializations must coincide`(engine: SerdeEngine) {
+        engine.serialize(
+            com.ing.zkflow.annotated.WrapsAttachmentConstraints.serializer(),
+            com.ing.zkflow.annotated.WrapsAttachmentConstraints()
+        ) shouldBe
+            engine.serialize(WrapsAttachmentConstraints.serializer(), WrapsAttachmentConstraints())
+    }
+}
