6dd6b807027f690f593c5679b241c70010ff01e0
Victor Ermolaev <victorermolaev@gmail.com>
2022-01-30 10:47:26 +0100

    Fix TestCommand in zkdapp-tester to be serializable.

--- zkdapp-tester/src/main/kotlin/zkdapp/TestCommand.kt
+++ zkdapp-tester/src/main/kotlin/zkdapp/TestCommand.kt
@@ -5,8 +5,12 @@ import com.ing.zkflow.common.zkp.metadata.ResolvedZKCommandMetadata
 import com.ing.zkflow.common.zkp.metadata.ResolvedZKTransactionMetadata
 import com.ing.zkflow.common.zkp.metadata.commandMetadata
 import com.ing.zkflow.common.zkp.metadata.transactionMetadata
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
+@Serializable
 class TestCommand: ZKTransactionMetadataCommandData {
+    @Transient
     override val transactionMetadata: ResolvedZKTransactionMetadata by transactionMetadata {
         commands { +TestCommand::class }
     }
