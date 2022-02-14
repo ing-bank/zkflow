19e227b56ed8b8e802e20461b2e02fee3748fc73
Victor Ermolaev <victor.ermolaev@ing.com>
2022-02-14 11:13:15 +0100

    Introduce abstractions enabling calculation of the serialization size.

---
+++ serialization-candidate/src/main/kotlin/com/ing/zkflow/serialization/FixedLengthSerialDescriptor.kt
@@ -0,0 +1,40 @@
+package com.ing.zkflow.serialization
+
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.elementDescriptors
+
+/**
+ * Attach size in bytes to kotlinx's [SerialDescriptor]
+ */
+class FixedLengthSerialDescriptor(
+    descriptor: SerialDescriptor,
+    val byteSize: Int,
+) : SerialDescriptor by descriptor
+
+/**
+ * Convenience function attempting to convert a [SerialDescriptor] to [FixedLengthSerialDescriptor] by inspection.
+ */
+fun SerialDescriptor.toFixedLengthSerialDescriptorOrThrow(parentSerialName: String? = null): FixedLengthSerialDescriptor {
+    val fullSerialName = "${if (parentSerialName != null) "$parentSerialName." else ""}$serialName"
+    return if (this is FixedLengthSerialDescriptor) {
+        this
+    } else {
+        // `this` is just a SerialDescriptor.
+        if (elementsCount == 0) {
+            // If no elements are present we cannot attach a size to the descriptor rendering it impossible
+            // to convert `SerialDescriptor` to `FixedLengthSerialDescriptor`
+            error(
+                """
+                `SerialDescriptor` `$fullSerialName` cannot be converted to `FixedLengthSerialDescriptor`
+                """.trimIndent()
+            )
+        } else {
+            FixedLengthSerialDescriptor(
+                this,
+                elementDescriptors.fold(0) { acc, serialDescriptor ->
+                    acc + serialDescriptor.toFixedLengthSerialDescriptorOrThrow(fullSerialName).byteSize
+                }
+            )
+        }
+    }
+}
