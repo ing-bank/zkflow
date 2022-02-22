289c010694011b71a1f976c6fd51e2cf0f2265d5
Matthijs van den Bos <matthijs.van.den.bos@ing.com>
2022-02-22 19:18:28 +0100

    Add simple contract DSL test

---
+++ protocol/src/test/kotlin/com/ing/zkflow/common/contract/SimpleContractTest.kt
@@ -0,0 +1,82 @@
+package com.ing.zkflow.common.contract
+
+import com.ing.serialization.bfl.annotations.FixedLength
+import com.ing.zkflow.common.contracts.ZKCommandData
+import com.ing.zkflow.common.contracts.ZKContractState
+import com.ing.zkflow.common.serialization.BFLSerializationScheme
+import com.ing.zkflow.common.serialization.BFLSerializationScheme.Companion.ZkCommandDataSerializerMap
+import com.ing.zkflow.common.zkp.metadata.ResolvedZKCommandMetadata
+import com.ing.zkflow.common.zkp.metadata.commandMetadata
+import com.ing.zkflow.serialization.bfl.serializers.AnonymousPartySerializer
+import com.ing.zkflow.testing.dsl.VerificationMode
+import com.ing.zkflow.testing.dsl.zkLedger
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.contracts.Contract
+import net.corda.core.identity.AnonymousParty
+import net.corda.core.transactions.LedgerTransaction
+import net.corda.testing.core.TestIdentity
+import net.corda.testing.node.MockServices
+import org.junit.jupiter.api.Test
+import java.util.Random
+
+class SimpleContractTest {
+    @Test
+    fun `Happy flow contract test must succeed`() {
+        val services = MockServices(listOf("com.ing.zkflow"))
+        val alice = TestIdentity.fresh("Alice").party.anonymise()
+        val bob = TestIdentity.fresh("Bobby").party.anonymise()
+
+        val aliceAsset = TestState(alice)
+
+        services.zkLedger {
+            zkTransaction {
+                input(LocalContract.PROGRAM_ID, aliceAsset)
+                output(LocalContract.PROGRAM_ID, aliceAsset.copy(owner = bob))
+                command(listOf(alice.owningKey, bob.owningKey), LocalContract.Move())
+                verifies(VerificationMode.MOCK)
+            }
+        }
+    }
+}
+
+class LocalContract : Contract {
+    companion object {
+        const val PROGRAM_ID = "com.ing.zkflow.common.contract.LocalContract"
+    }
+
+    override fun verify(tx: LedgerTransaction) {}
+
+    @Serializable
+    class Move : ZKCommandData {
+        init {
+            ZkCommandDataSerializerMap.register(this::class)
+        }
+
+        @Transient
+        override val metadata: ResolvedZKCommandMetadata = commandMetadata {
+            inputs {
+                private(TestState::class) at 0
+            }
+            outputs {
+                private(TestState::class) at 0
+            }
+            numberOfSigners = 2
+        }
+    }
+}
+
+@Serializable
+@BelongsToContract(LocalContract::class)
+data class TestState(
+    val owner: @Serializable(with = AnonymousPartySerializer::class) AnonymousParty,
+    val value: Int = Random().nextInt(1000)
+) : ZKContractState {
+    @FixedLength([1])
+    override val participants: List<@Serializable(with = AnonymousPartySerializer::class) AnonymousParty> = listOf(owner)
+
+    init {
+        BFLSerializationScheme.Companion.ZkContractStateSerializerMap.register(this::class)
+    }
+}
