4461aa776e206b9445d5c45b4d660f0627f3c49f
Peterpaul Klein Haneveld <peterpaul.klein.haneveld@ing.com>
2022-02-28 10:27:14 +0100

    Restore check for elementsCount, but allow classes without elements.

--- serialization-candidate/src/main/kotlin/com/ing/zkflow/serialization/FixedLengthSerialDescriptor.kt
+++ serialization-candidate/src/main/kotlin/com/ing/zkflow/serialization/FixedLengthSerialDescriptor.kt
@@ -1,6 +1,7 @@
 package com.ing.zkflow.serialization
 
 import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.StructureKind
 import kotlinx.serialization.descriptors.elementDescriptors
 
 /**
@@ -19,6 +20,11 @@ fun SerialDescriptor.toFixedLengthSerialDescriptorOrThrow(parentSerialName: Stri
     return if (this is FixedLengthSerialDescriptor) {
         this
     } else {
+        if (elementsCount == 0 && kind != StructureKind.CLASS) {
+            // If no elements are present we cannot attach a size to the descriptor rendering it impossible
+            // to convert `SerialDescriptor` to `FixedLengthSerialDescriptor`
+            error(" `SerialDescriptor` `$fullSerialName` cannot be converted to `FixedLengthSerialDescriptor`")
+        }
         FixedLengthSerialDescriptor(
             this,
             elementDescriptors.fold(0) { acc, serialDescriptor ->
