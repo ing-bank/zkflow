fc08f99ba4f8a957a43e2d88d29924238c97ab9f
Victor Ermolaev <victor.ermolaev@ing.com>
2022-03-24 16:23:23 +0100

    Update BestEffortTypeResolver.

--- compiler-plugin-arrow/src/main/kotlin/com/ing/zkflow/plugins/serialization/BestEffortTypeResolver.kt
+++ compiler-plugin-arrow/src/main/kotlin/com/ing/zkflow/plugins/serialization/BestEffortTypeResolver.kt
@@ -8,33 +8,55 @@ import org.jetbrains.kotlin.psi.KtImportList
 import org.jetbrains.kotlin.psi.KtVisitor
 
 class BestEffortTypeResolver(ktFile: KtFile) {
+    private val packageFqName = ktFile.packageFqName
     private val imports: KtImportList? = ktFile.importList
 
     init {
         imports?.accept(CollectWildcardImports, Unit)?.let { wildcardImports ->
-            SerdeLogger.log(wildcardImports.joinToString(separator = "\n"))
             require(wildcardImports.isEmpty()) {
                 "Wildcard imports are not permitted. Please specialize: ${wildcardImports.joinToString(separator = "\n")}"
             }
         }
     }
 
+    /**
+     * Attempt to resolve class name using the following strategies:
+     * - imported class is a compiled class in some module,
+     * - class with `simpleName` is a compiled class in this module,
+     * - class is present in this module but has not been compiled yet, and thus has been indexed,
+     * - class is imported from a different module (thus not indexed), but is not compiled yet
+     *  if none succeeds, return [AsIs]
+     */
     fun resolve(simpleName: String): BestEffortResolvedType {
-        imports?.accept(TypeResolverVisitor, simpleName)?.let {
-            try {
-                // Try if the class has been imported from `compiled` imports.
-                val kClass = Class.forName(it.toString()).kotlin
-                BestEffortResolvedType.FullyResolved(kClass)
-            } catch (e: ClassNotFoundException) {
-                // Class has been imported from the userspace.
-                null
-            }
-        }?.let { return it }
+        val import = imports?.accept(TypeResolverVisitor, simpleName)
+        SerdeLogger.log("Imports: ${imports?.text}\nSuitable import: $import")
 
+        // Case: imported class is a compiled class in some module.
+        import?.let { fqName ->
+            try {
+                val kClass = Class.forName(fqName.toString()).kotlin
+                return BestEffortResolvedType.FullyResolved(kClass)
+            } catch (_: ClassNotFoundException) { }
+        }
+
+        // Case: class with `simpleName` is a compiled class in this package.
+        try {
+            val kClass = Class.forName("$packageFqName.$simpleName").kotlin
+            return BestEffortResolvedType.FullyResolved(kClass)
+        } catch (_: ClassNotFoundException) { }
+
+        // Case: class is present in this module but has not been compiled yet,
+        //       and thus has been indexed.
         SerdeIndex[simpleName]?.let {
             return BestEffortResolvedType.FullyQualified(it.fqName, it.annotations)
         }
 
+        // Case: class is imported from a different module (thus not indexed), but is not compiled yet.
+        import?.let { fqName ->
+            return BestEffortResolvedType.FullyQualified(fqName, listOf())
+        }
+
+        // Else:
         return BestEffortResolvedType.AsIs(simpleName)
     }
 }
