9b9a5dc1405045f3a8c58def99dd79c8a6819f35
Peterpaul Klein Haneveld <peterpaul.klein.haneveld@ing.com>
2022-04-04 16:32:32 +0200

    Update design, documentation and add ZincUpgrade annotation

--- protocol/src/main/kotlin/com/ing/zkflow/common/versioning/Versioned.kt
+++ protocol/src/main/kotlin/com/ing/zkflow/common/versioning/Versioned.kt
@@ -1,6 +1,13 @@
 package com.ing.zkflow.common.versioning
 
 /**
- * A base interface that should be extended by a marker interface to group states or commands of different versions.
+ * A base interface that marks classes as versioned.
+ * This interface should be extended by a marker interface to group states or commands of different versions.
+ * A marker interface is an empty interface that extends this interface.
+ *
+ * ```kotlin
+ * interface Marker : Versioned
+ * data class Marked(...): Marker { ... }
+ * ```
  */
 interface Versioned
---
+++ protocol/src/main/kotlin/com/ing/zkflow/common/versioning/ZincUpgrade.kt
@@ -0,0 +1,27 @@
+package com.ing.zkflow.common.versioning
+
+import org.intellij.lang.annotations.Language
+
+/**
+ * Annotation specifying the [body] of a static zinc method on the enclosing type.
+ * The argument name of this method is derived from the parameter name in the constructor.
+ *
+ * ```kotlin
+ * interface MyType : Versioned
+ * @ZKP data class MyTypeV1(val a: Int) : MyType
+ * @ZKP data class MyTypeV2(val a: Int, val b: Int) : MyType {
+ *     @ZincUpgrade("Self::new(previous_version.a, 0 as i32)")
+ *     constructor(previousVersion: MyTypeV1) : this(previousVersion.a, 0)
+ * }
+ * ```
+ *
+ * ```rust
+ * impl MyTypeV2 {
+ *     fn upgrade_from(previous_version: MyTypeV1) -> Self {
+ *         Self::new(previous_version.a, 0 as i32)
+ *     }
+ * }
+ * ```
+ */
+@Target(AnnotationTarget.CONSTRUCTOR)
+annotation class ZincUpgrade(@Language("Rust")val body: String)
