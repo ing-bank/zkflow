053e4dc4e38afaa13044c0ca31174e89c6a9a4d6
Victor Ermolaev <victor.ermolaev@ing.com>
2022-04-29 12:41:50 +0200

    Add CBDC contract

--- zkdapp-tester/src/main/kotlin/com/example/contract/CBDCContract.kt
+++ zkdapp-tester/src/main/kotlin/com/example/contract/CBDCContract.kt
@@ -1,52 +1,18 @@
 package com.example.contract
 
+import com.example.token.cbdc.CBDCToken
 import com.ing.zkflow.annotations.ZKP
-import com.ing.zkflow.annotations.corda.EdDSA
 import com.ing.zkflow.common.contracts.ZKCommandData
-import com.ing.zkflow.common.contracts.ZKOwnableState
 import com.ing.zkflow.common.versioning.Versioned
-import com.ing.zkflow.common.versioning.ZincUpgrade
 import com.ing.zkflow.common.zkp.metadata.commandMetadata
-import net.corda.core.contracts.BelongsToContract
-import net.corda.core.contracts.CommandAndState
 import net.corda.core.contracts.Contract
 import net.corda.core.contracts.ContractClassName
-import net.corda.core.identity.AnonymousParty
 import net.corda.core.transactions.LedgerTransaction
 import org.intellij.lang.annotations.Language
-import java.util.Random
 
-
-class MockAssetContract : Contract {
+class CBDCContract : Contract {
     companion object {
-        const val ID: ContractClassName = "com.example.contract.MockAssetContract"
-    }
-
-    interface  MockAssetInterface: Versioned
-
-    @ZKP
-    @BelongsToContract(MockAssetContract::class)
-    data class MockAsset(
-        override val owner: @EdDSA AnonymousParty,
-        val value: Int = Random().nextInt()
-    ) : ZKOwnableState, MockAssetInterface {
-        @ZincUpgrade("Self::new(previous_version.owner, 0 as i32)")
-        constructor(previousVersion: MockAssetV1): this(previousVersion.owner, 0)
-        override val participants: List<AnonymousParty> = listOf(owner)
-
-        override fun withNewOwner(newOwner: AnonymousParty): CommandAndState =
-            CommandAndState(Move(), copy(owner = newOwner))
-    }
-
-    @ZKP
-    @BelongsToContract(MockAssetContract::class)
-    data class MockAssetV1(
-        override val owner: @EdDSA AnonymousParty,
-    ) : ZKOwnableState, MockAssetInterface {
-        override val participants: List<AnonymousParty> = listOf(owner)
-
-        override fun withNewOwner(newOwner: AnonymousParty): CommandAndState =
-            CommandAndState(Move(), copy(owner = newOwner))
+        const val ID: ContractClassName = "com.example.contract.CBDCContract"
     }
 
     interface  MoveInterface: Versioned
@@ -55,10 +21,10 @@ class MockAssetContract : Contract {
         override val metadata = commandMetadata {
             numberOfSigners = 2
             inputs {
-                any(MockAsset::class) at 0
+                any(CBDCToken::class) at 0
             }
             outputs {
-                private(MockAsset::class) at 0
+                private(CBDCToken::class) at 0
             }
             timeWindow = true
         }
@@ -87,7 +53,7 @@ class MockAssetContract : Contract {
         override val metadata = commandMetadata {
             numberOfSigners = 1
             outputs {
-                private(MockAsset::class) at 0
+                private(CBDCToken::class) at 0
             }
             timeWindow = true
         }
