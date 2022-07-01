4ec5363071d364b21ae15a7c406e51bb8e398c2a
Victor Ermolaev <victor.ermolaev@ing.com>
2022-07-01 13:09:51 +0200

    Remove all integration tests for the Arrow plugin.

--- integration-tests/compiler-plugin-arrow/src/test/kotlin/com/ing/zkflow/best_effort_type_resolver_test/two/TestMe.kt
+++
@@ -1,7 +0,0 @@
-package com.ing.zkflow.best_effort_type_resolver_test.two
-
-import com.ing.zkflow.annotations.ASCII
-import com.ing.zkflow.annotations.ZKP
-
-@ZKP
-data class TestMe(val string: @ASCII(5) String = "aa")
