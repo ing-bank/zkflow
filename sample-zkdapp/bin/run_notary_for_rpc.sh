ab254d982f01bcc8e99f0e10eb67b318beff074f
Matthijs van den Bos <matthijs@vandenbos.org>
2022-11-07 11:26:24 +0100

    Run sample zkdapp from published local jar

--- sample-zkdapp/bin/run_notary_for_rpc.sh
+++ sample-zkdapp/bin/run_notary_for_rpc.sh
@@ -1,4 +1,4 @@
-cd build/nodes/Issuer || exit
+cd build/nodes/ZKNotary || exit
 
 nohup java -Dexperimental.corda.customSerializationScheme=com.ing.zkflow.common.serialization.BFLSerializationScheme  \
     -jar corda.jar --allow-hibernate-to-manage-app-schema
